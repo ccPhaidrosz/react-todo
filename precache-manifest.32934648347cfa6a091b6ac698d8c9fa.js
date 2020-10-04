eb2db5501115aa3e577fe77fa718a56b7783ada5
ccPhaidrosz <arvylphoenix@gmail.com>
2020-10-04 21:57:32 +0200

    Updates

---
+++ precache-manifest.32934648347cfa6a091b6ac698d8c9fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55375f952ca0f54e18a733e5dcbbbb8c",
+    "url": "/ccPhaidrosz/react-todo/index.html"
+  },
+  {
+    "revision": "d885a8e8602e5513e469",
+    "url": "/ccPhaidrosz/react-todo/static/css/main.a423be14.chunk.css"
+  },
+  {
+    "revision": "3b51144e82c916fcf1fe",
+    "url": "/ccPhaidrosz/react-todo/static/js/2.03150a73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ccPhaidrosz/react-todo/static/js/2.03150a73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d885a8e8602e5513e469",
+    "url": "/ccPhaidrosz/react-todo/static/js/main.11360c4d.chunk.js"
+  },
+  {
+    "revision": "d97928033dc4e7f598a0",
+    "url": "/ccPhaidrosz/react-todo/static/js/runtime-main.4282ce27.js"
+  }
+]);
