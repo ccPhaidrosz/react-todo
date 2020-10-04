877cce91cef7683682bab81d12e302c4628379f1
ccPhaidrosz <arvylphoenix@gmail.com>
2020-10-04 20:46:04 +0200

    Basic functionality implemented

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import { v4 as uuidv4 } from 'uuid';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// ~ 1hour: https://www.youtube.com/watch?v=sBws8MSXN7A&list=PLtHfroUV9pt-hgHafaZHi4eDVBbPuwKoY&index=3
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: uuidv4(),
+        title: 'Finish todo app',
+        completed: false,
+      },
+      {
+        id: uuidv4(),
+        title: 'Crete own react project',
+        completed: true,
+      },
+      {
+        id: uuidv4(),
+        title: 'Deploy to netlify',
+        completed: false,
+      },
+    ],
+  };
+
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    });
+  };
+
+  delTodo = (id) => {
+    this.setState({
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+    });
+  };
+
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false,
+    };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <div className='container'>
+          <Header />
+          <AddTodo addTodo={this.addTodo} />
+          <Todos
+            todos={this.state.todos}
+            markComplete={this.markComplete}
+            delTodo={this.delTodo}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
